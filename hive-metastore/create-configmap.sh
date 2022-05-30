1647d5e4b38543401ab55c548c7b981764d7463c
Guilherme-Silveira <guilhermesilveira.s@hotmail.com>
2022-05-29 23:57:42 -0300

    fix hive metastore readme

--- hive-metastore/create-configmap.sh
+++ hive-metastore/create-configmap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+
+kubectl create secret generic my-s3-keys --from-literal=access-key=silveira --from-literal=secret-key=guilherme@123 --namespace bigdata
 kubectl create configmap metastore-cfg --namespace bigdata --dry-run=client --from-file=build/metastore-site.xml --from-file=build/core-site.xml -o yaml | kubectl apply -f -
